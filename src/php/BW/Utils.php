6c1234c45358f7e59cc3d98a570eab5bdd78323f
Alessandro Biavati <ale@alebiavati.com>
2014-02-20 14:33:59 -0800

    EDIT added getBlogId function

--- src/php/BW/Utils.php
+++ src/php/BW/Utils.php
@@ -635,4 +635,79 @@ class Utils {
 	}/* getFilename() */
 	
 
+
+	/**
+	 * get main site in a multisite installation
+	 *
+	 * @author Alessandro Biavati <ale@briteweb.com>
+	 * @package briteweb/utils
+	 * @since 1.0.0
+	 * @return (int) Main blog ID
+	 */
+	
+	static public function getMainBlogId()
+	{
+		if( BLOG_ID_CURRENT_SITE )
+			return BLOG_ID_CURRENT_SITE;
+		else
+			return 1;
+	
+	}/* getMainBlogId() */
+		
+
+	/**
+	 * Normalized search for a specific blog id.
+	 *
+	 * @author Alessandro Biavati <ale@briteweb.com>
+	 * @package briteweb/utils
+	 * @since 1.0.0
+	 * @param (int|string) $blog - Blog to find. It can be any of the following identifiers:
+	 *                             - 'network' -> returns 'network'
+	 *                             - 'site' -> returns the current site id
+	 *                             - 'main' -> returns the main site id
+	 *                             - (int) -> returns the input blog id
+	 *                             - 'example.org' -> the input is considered a domain, 
+	 *                             				 	  optional path can be specified
+	 *                             - null -> returns $_POST['blogId'] if set. 
+	 *                             			 Otherwise it returns the current site id
+	 * @return (int|string) either a blog id or "network"
+	 */
+	
+	static public function getBlogId( $blogId = null, $path = null )
+	{
+		// verify that we are in multisite. If not multisite, we return 1.
+		if ( !is_multisite() ) 
+			return 1;
+
+		if ( is_numeric( $blogId ) || $blogId === 'network' ) {
+
+			// do nothing
+
+		}elseif( $blogId == 'site' ){
+			
+			$blogId = get_current_blog_id();
+
+		}elseif ( $blogId === 'main' ) {
+
+			$blogId = static::getMainBlogId();
+
+		}elseif ( is_string( $blogId ) ) {
+
+			$blogId = get_blog_id_from_url( $blogId, $path );
+			
+		}elseif ( is_null( $blogId ) && isset( $_POST['blogId'] ) ) {
+
+			$blogId = intval( $_POST['blogId'] );
+
+		}else{
+
+			$blogId = get_current_blog_id();
+
+		}
+
+		return $blogId;
+	
+	}/* verifyBlogId() */
+
+
 }/* class Utils */
